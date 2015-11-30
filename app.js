92a7567d4528c75a3a357be29f690b39487c7b40
alika406 <ej3jp6h6cat@gmail.com>
2015-11-30 22:56:17 +0800

    Change mvc folder structrue

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var route = require('./route.js');
+var route = require('./application/route.js');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/application', 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // route
 route.run(app);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
