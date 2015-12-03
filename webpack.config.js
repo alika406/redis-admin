919d3edb5db3757c4bc57a1e8891663b7b65b68f
alika406 <ej3jp6h6cat@gmail.com>
2015-12-03 10:19:02 +0800

    Fix bundle scss to file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,41 @@
-module.exports = {
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config = {
+    // addVendor: function (type, name, path) {
+    //     var vendors = [];
+    //     this.resolve.alias[name] = path;
+    //     this.module.noParse.push(new RegExp('^' + name + '$'));
+    //     if (type === 'js'){
+    //         vendors.push(name);
+    //         this.entry.vendors = vendors;
+    //     }
+    // },
     entry: {
-	   index:"./assets/javascripts/index.js"
+	   index:"./assets/javascripts/index.js",
+       style:"./assets/stylesheets/style.js",
+       // vendors: "./assets/stylesheets/vendors.js",
     },
     output: {
         filename: "./public/javascripts/[name]-bundle.js"
     },
-module: {
+    resolve: {
+        alias: {}
+    },
+    module: {
+        noParse:[
+            // new RegExp("./assets/stylesheets/vendors.js"),
+            // new RegExp("./assets/stylesheets/scss/reset.css"),
+            // new RegExp("./assets/stylesheets/scss/bootstrap.min.css"),
+        ],
         loaders: [
-            { test: /\.scss$/, loader: 'style!css!sass!autoprefixer?{browsers:["> 5%", "ie >= 8", "Firefox < 20"]}' },
+            // { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", 'css-loader') },
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", 'css-loader!sass-loader!autoprefixer-loader?{browsers:["> 5%", "ie >= 8", "Firefox < 20"]}') },
             { test: /\.jsx?$/, loader: 'babel', exclude: /(node_modules|bower_components)/ }
         ]
-    }
+    },
+    plugins : [
+        new ExtractTextPlugin('./public/stylesheets/[name].css')
+    ]
 };
+
+module.exports = config;
